b5a2543ea911ee839b540211efcee074d7ce9149
zhenxuan98 <zhenxuantan98@gmail.com>
2021-12-06 04:03:38 +0800

    tcp serverr

--- RoboSensei/server.js
+++ RoboSensei/server.js
@@ -4,7 +4,35 @@ const mongoose = require('mongoose');
 const session = require("express-session");
 const passport = require("passport");
 var cookieParser = require('cookie-parser')
+const net = require('net')
+const fs = require('fs')
 
+const tcpServer = net.createServer(function (client) {
+  console.log("MSP432 Is Connected")
+  client.setEncoding('utf-8')
+  client.on('data', function (data) {
+    console.log(data)
+    if (data == "Hello") {
+      try {
+        const data = fs.readFileSync('assets/MSP432 Files/commands.txt', 'utf8')
+        fs.truncateSync( 'assets/MSP432 Files/commands.txt', 0 )
+        client.end("FLR")
+      } catch (err) {
+        console.log(err)
+      }
+    } else {
+      // console.log(JSON.parse(data))
+
+    }
+  })
+  client.on('end', function () {
+    console.log("MSP432 Disconnected")
+  })
+
+});
+tcpServer.listen(5000,()=>{
+  console.log("TCP Server Listening on Port:5000")
+})
 const app = express();
 app.use(cookieParser())
 app.use(express.json())
